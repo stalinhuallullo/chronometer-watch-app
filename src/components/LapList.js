21d6481c587aa08a3252b8ed02c26c378456754d
StalinHuallullo <antonyhuallullo@gmail.com>
2022-02-24 21:31:13 -0500

    feat(LapList) - Implementando component para listar los tiempos

---
+++ src/components/LapList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const LapList = () => {
+  return (
+    <div >
+		<div id="cajaResultados">
+			<h1>Resultados: </h1>
+			<ol id="resultados">
+			</ol>
+		</div>
+	</div>
+  )
+}
